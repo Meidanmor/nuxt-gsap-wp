e3658f247ddeb3418c80907e1487f6efe27e6dad
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-09-04 19:38:21 +0300

    Update _slug.vue

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -3,7 +3,7 @@
   <main class="post individual">
   <div class="modalPostImage"> <img :src="post.images.large" width="500" height="500"/></div>
   
-  <div class="modal__container" role="dialog" aria-modal="true">{{ post.title.rendered }} <div v-html="post.content.rendered"></div><div class="didYouLike"><h3> אהבת את הפרויקט? </h3> <div class="contain"><div class="yes"><a href="javascript:void(0)"> כן! </a> </div><div class="no"> <a href="javascript:void(0)"> לא ממש </a> </div> </div> </div> </div>
+  <div class="modal__container" role="dialog" aria-modal="true"><h1>{{ post.title.rendered }}</h1> <div v-html="post.content.rendered"></div><div class="didYouLike"><h3> אהבת את הפרויקט? </h3> <div class="contain"><div class="yes"><a href="javascript:void(0)"> כן! </a> </div><div class="no"> <a href="javascript:void(0)"> לא ממש </a> </div> </div> </div> </div>
   
   </main>
   </div>
@@ -227,64 +227,106 @@ document.querySelector('.scrollToTop').style.zIndex = "9";
 </script>
 
 <style lang="scss" scoped>
-div.wrapper{
-  padding-top: 0;
+.discreption {
+    background: #fff;
+    color: #000;
+    padding: 20px;
+    margin-left: 50px;
+	font-size: 18px;
 }
-main.post {
-    background: aliceblue;
-display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-content: center;
-    flex-wrap: wrap;
-    align-items: center;
-    height: 100%;
+.modal__container {
+    width: 100%;
+/*    background: linear-gradient(45deg, transparent -10% , black 76%);*/
+    padding: 0px;
+    overflow: visible;
+	    position: relative;
 }
+h1 {
+	font-family: 'clashdisplay-semibold';
+    font-size: 35px;
+	line-height: 1;
+	margin: 0 0 45vh;
+	padding-top: 25vh;
+	-webkit-text-stroke: 1px #fff;
+	color: transparent;
+	text-align: center;
 
-.mainWrapper {
+}
+ .contents {
+    width: 100%;
+    font-size: 22px;
+    color: white;
+}
+.modalPostImage {
+    position: fixed;
     height: 100vh;
-    background: aliceblue;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    flex-wrap: wrap;
-    align-items: center;
-    width: 100%;
-
-}
-.mainWrapper img {
-    position: absolute;
-    top: 0;
-    opacity: .7;
+    width: 100vw;
     left: 0;
+    top: 0;
+    z-index: -1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: -webkit-center;
+    background: #222;
+}
+
+.modalPostImage img{
+filter: drop-shadow(5px 5px 5px black);
+}
+.theVideo{
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+ .pin-spacer{
+		pointer-events: none
+}
+ figure.wp-block-video {
+position: fixed;	
+width: fit-content;
+}
+
+ figure.wp-block-video:before {
+    width: 100%;
     height: 100%;
-    object-fit: contain;
-    max-height: 100vh;
-}
-
-section {
-  color: #444;
-}
-
-.date {
-  position: absolute;
-    left: 10px;
-    bottom: 5px;
-}
-section.postContents{
-    padding: 30px;
-    background-image: linear-gradient(0, #fff 98%, transparent);
-    width: 100%;
     position: absolute;
-    top:85%;
-    color: black;
+    display: block;
+    z-index: 9999;
+    content: '';
+    top: 0;
+    left: 0;
+    background: url(https://spite.meidanm.com/wp-content/uploads/2022/08/apple-iphone-13-2021-medium.png);
+    background-size: 94%;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+ video {
+    max-height: 400px;
+    object-fit: contain;
+    background: transparent;
+    width: fit-content;
+    max-width: 700px;
+    height: 40vh;
+    border-radius: 28px;
+    opacity: 1;
+	margin-top: 5px;
+    padding: 5px 8px 0;
+}
 
+ img.attachment-large.size-large.wp-post-image {
+    max-height: 300px;
+    max-width: 200px;
+    background: transparent;
+    height: 100%;
+}
 
-  h1{
-    font-size: 40px;
-    margin-bottom: 0;
-    width: 100%;
-    padding: 0 0 30px;
-  }
+figure.thePostGallery{
+	max-width: 100%;
+}
+ figure{
+	margin: 0;
 }
 </style>
