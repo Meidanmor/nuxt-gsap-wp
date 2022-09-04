a6b71b93916f4a97517f79db400f6da6b7d317e1
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-09-04 18:52:35 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -33,7 +33,7 @@
 
 <script>
 import AppMasthead from "@/components/AppMasthead.vue";
-import { bodyScrollBar } from "@/layouts/default.vue";
+import Scrollbar from 'smooth-scrollbar';
 import { gsap, Power4 } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
@@ -68,6 +68,25 @@ ScrollTrigger.refresh();
   },
   methods: {
 startAnim(){
+const scroller = document.querySelector('.theNuxt');
+
+const bodyScrollBar = Scrollbar.init(scroller, { damping: 0.1, alwaysShowTracks: false });
+window.onbeforeunload = function(e){
+  sessionStorage.setItem('bodyScrollBar', bodyScrollBar.offset.y);
+};
+ScrollTrigger.scrollerProxy('.theNuxt', {
+  scrollTop(value) {
+    if (arguments.length) {
+      bodyScrollBar.scrollTop = value;
+    }
+    return bodyScrollBar.scrollTop;
+  }
+});
+
+bodyScrollBar.addListener(ScrollTrigger.update);
+  console.log(bodyScrollBar);
+
+ScrollTrigger.defaults({ scroller: scroller });
 if(document.querySelector('a.showProjects')){
 document.querySelector('a.showProjects').addEventListener('click', function(e){
 var heroHeight = document.querySelector('.hero').clientHeight;
