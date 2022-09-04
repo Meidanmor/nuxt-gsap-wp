e729addbc9b4496548b177ee52611c01df68749f
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-09-04 18:51:16 +0300

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,7 +23,7 @@ export default {
   },
   mounted(){
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-  this.addSmoothScrollBar();
+  //this.addSmoothScrollBar();
   },
   methods:{
    addSmoothScrollBar(){
