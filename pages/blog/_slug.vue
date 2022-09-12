c4e3ff9e5d13b89db7e511b5575d65f42bc4a5ee
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-09-12 13:51:16 +0300

    Update _slug.vue

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -35,7 +35,7 @@ export default {
   data() {
     return {
       slug: this.$route.params.slug,
-      prevId: post.prev_next.prev
+      prevId:   {{ post.prev_next.prev }}
     };
   },
    head() {
