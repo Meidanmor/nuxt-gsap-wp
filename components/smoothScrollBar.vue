56f065821a29a3fac4dd1cd7484077c090809511
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-09-14 12:59:24 +0300

    Create smoothScrollBar.vue

---
+++ components/smoothScrollBar.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+mounted(){
+this.addSmoothScrollBar();
+console.log('mnted');
+},
+ methods: {
+ addSmoothScrollBar(){
+const scroller = document.querySelector('.theNuxt');
+const bodyScrollBar = Scrollbar.init(scroller, { damping: 0.1, alwaysShowTracks: false });
+window.onbeforeunload = function(e){
+  sessionStorage.setItem('bodyScrollBar', bodyScrollBar.offset.y);
+};
+ScrollTrigger.scrollerProxy('.theNuxt', {
+  scrollTop(value) {
+    if (arguments.length) {
+      bodyScrollBar.scrollTop = value;
+    }
+    return bodyScrollBar.scrollTop;
+  }
+});
+bodyScrollBar.addListener(ScrollTrigger.update);
+  console.log(bodyScrollBar);
+ScrollTrigger.defaults({ scroller: scroller });
+ }
+ },
+};
+
+</script>
