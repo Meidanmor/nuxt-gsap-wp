440e935f495e816bafb02aab8cf739742661e623
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-09-14 12:14:45 +0300

    Update _slug.vue

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -35,7 +35,7 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
 
 export default {
-   middleware: 'scrollpos',
+  // middleware: 'scrollpos',
 
   computed: {
     posts() {
