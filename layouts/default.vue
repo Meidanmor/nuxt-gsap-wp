364973113f26b93b1207063705f20f7160e0291f
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-09-14 11:42:37 +0300

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,13 +26,21 @@ export default {
   this.addSmoothScrollBar();      
     },
   },
-  beforeRouteEnter (to, from, next) {
-   console.log(to);
-  },
-
- beforeRouteUpdate (to, from, next) {
-   console.log(to, from, next);
-  },
+router.beforeResolve(async to => {
+  if (to.meta.requiresCamera) {
+    try {
+      await askForCameraPermission()
+    } catch (error) {
+      if (error instanceof NotAllowedError) {
+        // ... handle the error and then cancel the navigation
+        return false
+      } else {
+        // unexpected error, cancel the navigation and pass the error to the global handler
+        throw error
+      }
+    }
+  }
+}),
   components: {
     AppNav
   },
