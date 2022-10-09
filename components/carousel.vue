4ab40295719b40d33e40a7635905f149dbb41768
Meidanmor <85040197+Meidanmor@users.noreply.github.com>
2022-10-09 14:57:06 +0300

    Create carousel.vue

---
+++ components/carousel.vue
@@ -0,0 +1,23 @@
+<template>
+<carousel-3d>
+  <slide :index="0">
+    Slide 1 Content
+  </slide>
+  <slide :index="1">
+    Slide 2 Content
+  </slide>
+    <slide :index="2">
+    Slide 3 Content
+  </slide>
+</carousel-3d>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue3-carousel-3d';
+import "vue3-carousel-3d/dist/index.css";
+export default {
+  components: { 
+Carousel3d, Slide
+   },
+};
+</script>
